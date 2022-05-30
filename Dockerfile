6f721510590c774df143dbf5788d8edf79eab3b0
Adam Tamics <57900951+adamtamics@users.noreply.github.com>
2022-05-30 12:41:57 +0200

    Add fallback logic for PORT

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
 FROM golang:1.14.9-alpine AS builder
+RUN mkdir /build
+ADD go.mod main.go /build/
+WORKDIR /build
 RUN go build -o app -v .
 
+# FROM alpine
+# RUN adduser -S -D -H -h /app appuser
+# USER appuser
+# COPY --from=builder /build/main /app/
+# WORKDIR /app
 CMD ["./app"]
--- main.go
+++ main.go
@@ -18,6 +18,14 @@ func main() {
 		fmt.Fprintf(w, "Hi")
 	})
 
-	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), nil))
+	log.Fatal(http.ListenAndServe(getenv("PORT", ":8081"), nil))
 
+}
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+			return fallback
+	}
+	return value
 }
